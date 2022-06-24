1e7145674f3ce744de90272960b8f28d9327358d
Philippe Charrière <ph.charriere@gmail.com>
2022-06-24 15:30:23 +0000

    📝 save

--- 10-rust-dom/hello/src/lib.rs
+++ 10-rust-dom/hello/src/lib.rs
@@ -32,14 +32,16 @@ struct Constraints {
     max_velocity: f64
 }
 
+#[derive(Clone, Copy)]
 struct Cow {
-    nick_name: String,
+    //nick_name: String,
     x: f64,
     y: f64,
     constraints: Constraints,
     x_velocity: f64,
     y_velocity: f64
 }
+
 
 impl Cow {
     fn moving(&mut self) {
@@ -70,7 +72,7 @@ impl Cow {
         (dist_x*dist_x + dist_y*dist_y).sqrt()
     }
 
-    fn move_away(&mut self, boids: vec![Cow], minDistance: f64) {
+    fn move_away(&mut self, boids: Vec<Cow>, min_distance: f64) {
         let mut distance_x = 0.0;
         let mut distance_y = 0.0;
         let mut num_close = 0.0;
@@ -78,14 +80,78 @@ impl Cow {
         for boid in boids.iter() {
 
             if boid.x == self.x && boid.y == self.y {
-                print!("...")
+                continue;
             }
 
+            let mut distance = self.distance(*boid);
+
+            if distance < min_distance {
+                num_close +=1.0;
+                let mut xdiff = self.x - boid.x;
+                let mut ydiff = self.y - boid.y;
+
+                if xdiff >= 0.0 {
+                    xdiff = min_distance.sqrt() - xdiff;
+                } else if xdiff < 0.0 {
+                    xdiff = -min_distance.sqrt() - xdiff;
+                }
+    
+                if ydiff >= 0.0 {
+                    ydiff = min_distance.sqrt() - ydiff;
+                } else if ydiff < 0.0 {
+                    ydiff = -min_distance.sqrt() - ydiff;
+                }
+
+                distance_x += xdiff;
+                distance_y += ydiff;
+
+            }
+
+            if num_close == 0.0 {
+
+            } else {
+                self.x_velocity -= distance_x / 5.0;
+                self.y_velocity -= distance_y / 5.0;
+            }
+        }
+    }
+
+    fn move_closer(&mut self, boids: Vec<Cow>, distance: f64) {
+        if boids.len() < 1 {
+            // do nothing
+        } else {
+            let mut avg_x = 0.0;
+            let mut avg_y = 0.0;
+
+            for boid in boids.iter() {
+                if boid.x == self.x && boid.y == self.y {
+                    continue;
+                }
+                if self.distance(*boid) > distance {
+                    continue;
+                }
+
+                avg_x += self.x - boid.x;
+                avg_y += self.y - boid.y;
+            }
+
+            avg_x /= boids.len() as f64;
+            avg_y /= boids.len() as f64;
+
+            let avg_distance = (avg_x*avg_x+avg_y*avg_y).sqrt() * -1.0;
+
+            if avg_distance == 0.0 {
+             
+            } else {
+                self.x_velocity = (self.x_velocity + (avg_x/avg_distance) * 0.15).min(self.constraints.max_velocity);
+                self.y_velocity = (self.y_velocity + (avg_y/avg_distance) * 0.15).min(self.constraints.max_velocity);
+
+            }
 
         }
-
-
     }
+
+
 
 }
 
@@ -107,25 +173,27 @@ pub fn run() -> Result<(), JsValue> {
 		max_velocity: 5.0
     };
     let mut bob = Cow {
-        nick_name: String::from("Bob"),
+        //nick_name: String::from("Bob"),
         x:10.0,
         y:10.0,
         constraints: constraints, 
         x_velocity: 1.0, 
         y_velocity: -1.0
     };
+    
     bob.moving();
-    bob.moving();
-    bob.moving();
+    //bob.moving();
+    //bob.moving();
 
     let mut sam = Cow {
-        nick_name: String::from("Sam"),
+        //nick_name: String::from("Sam"),
         x:10.0,
         y:10.0,
         constraints: constraints, 
         x_velocity: 1.0, 
         y_velocity: -1.0
     };
+
 
     //bob.x =23.0;
     //bob.x_velocity = 4.0;
@@ -139,8 +207,18 @@ pub fn run() -> Result<(), JsValue> {
     let h2 = document.create_element("h2")?;
     h2.set_text_content(Some(&bob.x.to_string()));
 
+    let cows_list = vec![bob, sam];
+    bob.move_away(cows_list, 15.0);
+
     let h2_again = document.create_element("h2")?;
     h2_again.set_text_content(Some(&bob.distance(sam).to_string()));
+
+    
+
+    //let mut cows_list: Vec<Cow> = vec![bob];
+
+    
+
 
     body.append_child(&h1)?;
     body.append_child(&h2)?;
